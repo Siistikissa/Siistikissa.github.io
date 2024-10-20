a751840c29f15aa0d1eb96e8ab47504c7d016e49
Veikka Halonen <halonenveikka@gmail.com>
2024-10-20 19:23:04 +0300

    shuffle js

--- scripts.js
+++ scripts.js
@@ -17,36 +17,35 @@ async function getArt(file){
     try{
     let myObject = await fetch(file);
     let myArt = await myObject.text();
-    console.log(myArt)
     //document.getElementById("ascii").style.visibility = "visible";
-    document.getElementById("ascii").innerHTML = myArt;
+    countBraincells(file);
     } catch(error){
         console.log("Error fetching data: ", error);
     }
 }
 async function countBraincells(file) {
     try{
-        var myObject = await fetch(file);
-        } 
+        let myObject = await fetch(file);
+        let mybrain = await myObject.text();
+        let charCount = {};
+        for(let char of mybrain){
+            if(charCount[char]){
+                charCount[char]++;
+            }
+            else{
+                charCount[char] = 1;
+            }
+        }
+        let brainText;
+        for (let char in charCount) {
+            brainText.concat(`${char} occurs ${charCount[char]} times\n`);
+        }
+        document.getElementById("braincell").style.visibility = "visible";
+        document.getElementById("braincell").innerHTML = brainText;        
+    } 
     catch(error){
         console.log("Error fetching data: ", error);
     }
-    let mybrain = await myObject.text();
-    let charCount = {};
-    for(let char of mybrain){
-        if(charCount[char]){
-            charCount[char]++;
-        }
-        else{
-            charCount[char] = 1;
-        }
-    }
-    let brainText;
-    for (let char in charCount) {
-        brainText.concat(`${char} occurs ${charCount[char]} times\n`);
-    }
-    document.getElementById("braincell").style.visibility = "visible";
-    document.getElementById("braincell").innerHTML = brainText;
 }
 let catPetStatus = false;
 function petCat(params) {
